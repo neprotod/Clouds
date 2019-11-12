f36f3772dea33c8b010e923633b6657cb51673a0
Alexander Podlesnyi <53093281+neprotod@users.noreply.github.com>
2019-11-12 19:11:04 +0200

    Fix class and animation

--- src/index.js
+++ src/index.js
@@ -1,122 +1,210 @@
-var clouds = {
-  //Свойства
+class Sky {
+  constructor({
+    speedStart = 30,
+    speedEnd = 80,
+    zIndexStart = 2,
+    zIndexEnd = 11
+  } = {}) {
+    this.speedStart = speedStart;
+    this.speedEnd = speedEnd;
+    this.zIndexStart = zIndexStart;
+    this.zIndexEnd = zIndexEnd;
 
-  // диапазон скорости
-  speed1: 30,
-  speed2: 80,
+    // All object Cloud
+    this.allClouds = [];
 
-  // Для расчета случайного числа
-  getRandom: function(min_random, max_random) {
-    var range = max_random - min_random + 1;
-    return Math.floor(Math.random() * range) + min_random;
-  },
+    //Parent element
+    this.parent = document.getElementById("clouds_place");
 
-  init: function(div, left1, left2, top1, top2) {
-    var objName = div.id;
-    //Начальные данные
-    var randomLeft = this.getRandom(left1, left2);
-    var randomZindex = this.getRandom(2, 11);
-    //Значение ширины элемента
-    var divWidth;
+    this.clientWidth = this.parent.clientWidth;
 
-    var randomSpeed = this.getRandom(this.speed1, this.speed2);
-
-    var randomTop = this.getRandom(top1, top2);
-
-    var object = this;
-
-    this[objName] = {
-      time: "",
-      init: 1,
-
-      randomLeft: randomLeft,
-      randomZindex: randomZindex,
-      divWidth: divWidth,
-      randomSpeed: randomSpeed,
-      randomTop: randomTop,
-
-      object: object,
-
-      div: div,
-      // характиристики
-      top1: top1,
-      top2: top2,
-
-      clearLeft: function() {
-        clearTimeout(this.time);
-        this.div.style.left = -this.divWidth + "px";
-        this.randomTop = this.object.getRandom(this.top1, this.top2);
-        this.randomSpeed = this.object.getRandom(
-          this.object.speed1,
-          this.object.speed2
-        );
-        this.randomZindex = this.object.getRandom(2, 11);
-        this.goClouds(1);
-      },
-
-      goClouds: function(a) {
-        var obj = this;
-
-        var body = document.body.clientWidth;
-        var left = this.div.style.left;
-        left = parseInt(left, 10);
-
-        // Если элемент достиг конца
-        if (left > body) {
-          return this.clearLeft();
+    // Create animation function
+    window.requestAnimFrame = (function(callback) {
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+          window.setTimeout(callback, 300);
         }
+      );
+    })();
+  }
 
-        /***********/
-        if (!left && left !== 0) {
-          left = this.randomLeft;
-          this.div.style.top = this.randomTop + "px";
-          this.div.style.zIndex = this.randomZindex;
-          this.divWidth = this.div.clientWidth;
-        } else {
-          left += 1;
-        }
+  /**
+   * For prepare random number
+   *
+   * @param {number} minRandom
+   * @param {number} maxRandom
+   * @return {number} random namber
+   */
+  getRandom(minRandom, maxRandom) {
+    var range = maxRandom - minRandom + 1;
+    return Math.floor(Math.random() * range) + minRandom;
+  }
 
-        if (a === 1) {
-          this.div.style.top = this.randomTop + "px";
-          this.div.style.zIndex = this.randomZindex;
-          this.divWidth = this.div.clientWidth;
-        }
+  /**
+   * Connection all cloud in clouds_place element
+   *
+   * @return {boolean} true when all good
+   */
+  connectionAllClouds() {
+    let clouds = this.parent.querySelectorAll(".cloud");
+    clouds.forEach(elem => {
+      this.addClouds(elem);
+    });
 
-        this.div.style.left = left + "px";
+    return true;
+  }
 
-        obj.time = setTimeout(function() {
-          obj.goClouds();
-        }, obj.randomSpeed);
-      }
+  /**
+   * Connection cloud
+   *
+   * @param {Object} div           element
+   * @param {Object} Obj           params
+   * @param {number} Obj.leftStart start left border
+   * @param {number} Obj.leftEnd   end left border
+   * @param {number} Obj.topStart  start top border (default -50)
+   * @param {number} Obj.topEnd    end top border (default 300)
+   * @return {void}
+   */
+  addClouds(
+    div,
+    { leftStart = 0, leftEnd = null, topStart = -50, topEnd = 300 } = {}
+  ) {
+    if (!div) throw new Error("Element is null");
+    //Default data
+    if (!leftEnd) {
+      leftEnd = this.clientWidth;
+    }
+    const randomLeft = this.getRandom(leftStart, leftEnd);
+    const randomZindex = this.getRandom(this.zIndexStart, this.zIndexEnd);
+
+    const randomSpeed = this.getRandom(this.speedStart, this.speedEnd);
+    const randomTop = this.getRandom(topStart, topEnd);
+
+    // Sample parent class
+    const object = this;
+
+    // Time to animation
+    const time = Date.now();
+
+    // Config for cloud
+    const config = {
+      randomLeft,
+      randomZindex,
+      randomSpeed,
+      randomTop,
+      topStart,
+      topEnd,
+      div,
+      time,
+      object
     };
 
-    this[objName].goClouds();
+    this.allClouds.push(new Cloud(config));
   }
-};
 
-//***********//
+  /**
+   * Start animation
+   *
+   * @return {void}
+   */
+  start() {
+    this.clientWidth = this.parent.clientWidth;
 
-//**********//
-//Наименование облаков
-var claud1 = document.getElementById("clouds1");
-var claud2 = document.getElementById("clouds2");
-var claud3 = document.getElementById("clouds3");
-var claud4 = document.getElementById("clouds4");
-var claud5 = document.getElementById("clouds5");
-var claud6 = document.getElementById("clouds6");
-var claud7 = document.getElementById("clouds7");
-var claud8 = document.getElementById("clouds8");
-var claud9 = document.getElementById("clouds9");
+    this.allClouds.forEach(elem => {
+      elem.goClouds();
+    });
 
-// Запуск функции облаков
-clouds.init(claud1, 1, 300, -50, 300);
-clouds.init(claud2, 410, 800, -50, 300);
-clouds.init(claud3, 950, 1500, -50, 300);
+    window.requestAnimationFrame(() => {
+      this.start();
+    });
+  }
+}
 
-clouds.init(claud4, 100, 1500, -50, 300);
-clouds.init(claud5, 50, 300, -50, 300);
-clouds.init(claud6, 450, 800, -50, 300);
+class Cloud {
+  /**
+   *
+   * @param {Object} config configuration this cloud
+   */
+  constructor(config) {
+    for (let key in config) {
+      this[key] = config[key];
+    }
 
-clouds.init(claud7, 400, 800, -50, 300);
-clouds.init(claud8, 900, 1500, -50, 300);
-clouds.init(claud9, 100, 300, -50, 300);
+    // With element
+    this.divWidth = this.div.clientWidth;
+  }
+
+  /**
+   * Clear cloud possition before next stap animation
+   *
+   * @return {void}
+   */
+  clearCloupPosition() {
+    this.div.style.left = -this.divWidth + "px";
+    this.randomTop = this.object.getRandom(this.topStart, this.topEnd);
+    this.randomSpeed = this.object.getRandom(
+      this.object.speedStart,
+      this.object.speedEnd
+    );
+
+    // Add data frame
+    this.time = Date.now();
+
+    this.randomZindex = this.object.getRandom(
+      this.object.zIndexStart,
+      this.object.zIndexEnd
+    );
+    this.goClouds(true);
+  }
+
+  /**
+   * Step animation cloud
+   *
+   * @param {boolean} force if need to force replace value in cloud
+   */
+  goClouds(force = false) {
+    if (Date.now() >= this.time) {
+      // Time to animation
+      this.time = Date.now() + this.randomSpeed;
+
+      const clientWidth = this.object.clientWidth;
+      let left = "";
+      if (this.div.style.left) {
+        left = this.div.style.left;
+        left = parseInt(left, 10);
+      }
+
+      // Если элемент достиг конца
+      if (left > clientWidth) {
+        return this.clearCloupPosition();
+      }
+
+      if (!left && left !== 0) {
+        left = this.randomLeft;
+        this.div.style.top = this.randomTop + "px";
+        this.div.style.zIndex = this.randomZindex;
+        this.divWidth = this.div.clientWidth;
+      } else {
+        left += 1;
+      }
+
+      if (force === true) {
+        this.div.style.top = this.randomTop + "px";
+        this.div.style.zIndex = this.randomZindex;
+        this.divWidth = this.div.clientWidth;
+        force = false;
+      }
+
+      this.div.style.left = left + "px";
+    }
+  }
+}
+
+const sky = new Sky();
+sky.connectionAllClouds();
+sky.start();
